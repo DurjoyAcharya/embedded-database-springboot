a0525c5271940577c58dee868721a0920e61e633
DurjoyAcharya <da-durjoy@outlook.com>
2023-02-20 18:42:21 +0600

    derby concept

---
+++ src/main/java/com/example/embedded_database_spring_boot/EmbeddedDatabaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.embedded_database_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedDatabaseSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedDatabaseSpringBootApplication.class, args);
+    }
+
+}
