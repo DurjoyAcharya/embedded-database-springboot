a76dc3374f2406fe58451263ae0e58b3468caaaf
DurjoyAcharya <da-durjoy@outlook.com>
2023-02-20 18:44:15 +0600

    derby concept

---
+++ src/test/java/com/example/embedded_database_spring_boot/EmbeddedDatabaseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.embedded_database_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedDatabaseSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
